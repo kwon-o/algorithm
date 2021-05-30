3a980131b9ed035b3f43138a3761d4b116fbfd20
kwon-o <koj1228853@gmail.com>
2021-05-30 13:50:18 +0900

    2021/05/30

---
+++ DataStructures/2517_달리기/20210530_2517_kwon.py
@@ -0,0 +1,11 @@
+import heapq
+N,*l=map(int,open("input.txt", "r").read().split())
+heap=[]
+for i,n in enumerate(l):
+    heapq.heappush(heap, (i,n))
+for i in heap:
+    ans=1
+    for j in range(i[0]):
+        if i[1] <= heap[j][1]:
+            ans+=1
+    print(ans)
