ce21da46f869a8b3269131365711520fb0523302
kwon-o <koj1228853@gmail.com>
2021-12-05 21:02:17 +0900

    2021/12/05 upload file

---
+++ DataStructures/4949_균형잡힌세상/20211205_4949_kwon.py
@@ -0,0 +1,12 @@
+while 1:
+    l=input()
+    if'.'==l:break
+    t=''
+    for i in l:
+        if i in['(',')','[',']']:
+            t+=i
+    while 1:
+        s = t.replace('()','').replace('[]','')
+        if s and s == t:print('no');break
+        elif not s:print('yes');break
+        else:t=s
