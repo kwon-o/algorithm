a97f148c9a1b85dd977f12024ff0ab62ebee3942
LeeeeHo02 <89404338+LeeeeHo02@users.noreply.github.com>
2021-12-20 21:28:28 +0900

    Create 11279_1220_lee.py

---
+++ DataStructures/11279_최대힙/11279_1220_lee.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+nums = int(input())
+heap = []
+for i in range(nums):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        if len(heap) >= 1:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        # (우선 순위, 값)
+        heapq.heappush(heap, (-num, num))
